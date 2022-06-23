fc161f423f1f18c81ce7c2e80a8b37c2ce576766
Orllewin <orllewin@fastmail.uk>
2022-06-23 19:50:02 +0100

    ...

--- Source/Scenes/Start/start_scene.lua
+++ Source/Scenes/Start/start_scene.lua
@@ -1,6 +1,4 @@
 import 'Coracle/coracle'
-import 'Coracle/Particles/starfield'
-import 'Coracle/Particles/starbackground'
 
 class('StartScene').extends()
 
@@ -13,33 +11,74 @@ function StartScene:init()
 	 end
 	 
 	 self.startImage = playdate.graphics.image.new("images/splash_scene_background")
-	 self.degrees = 0
-	 self.starBackground = StarBackground(60, 1)
-	 self.starfield = Starfield(30, 4, 12, 0.07)
+
+	 --Oldschool 3D
+	 self.showCalxLogo = true
+	 self.t = 0
+	 self.donut = true
 	 playdate.graphics.setColor(white)
 end
 
+local cos <const> = math.cos
+local sin <const> = math.sin
+
 function StartScene:draw()
 	background(black)
-	self.startImage:draw(0, 0)
-	self.starBackground:draw(self.degrees)
-	self.starfield:draw(self.degrees * -1)
-	
-	self.degrees = self.degrees += 2
-	
-	if(self.degrees >= 360)then
-		self.degrees = 0
+	if(self.showCalxLogo)then
+		self.startImage:draw(0, 0)
 	end
-	
+
 	text("Press A to start", 10, 220)
 
 	if(aPressed())then
 		self:onDismissStart()
 	end
 	
+	if(bPressed())then
+		if(self.showCalxLogo)then
+			self.showCalxLogo = false
+		else
+			self.showCalxLogo = true
+		end
+	end
+	
+	if(upPressed())then
+		if(self.donut)then
+			self.donut = false
+		else
+			self.donut = true
+		end
+	end
+	
 	if(AUTO_START)then
 		self:onDismissStart()
 	end
+	
+	--Oldschool 3D
+	self.t += 0.05
+	setWhite()
+	fill(0.4)
+	local donut = self.donut
+	for i=90,0, -1  do
+
+		local q = (i * i)
+		local Q = sin(q)
+		
+		local b
+		if(donut)then
+			b = i % 6 + self.t + i
+		else
+			b = i % 6 + self.t
+		end
+
+		local p = i + self.t
+		local z = 6 + cos(b) * 3 + cos(p) * Q
+		local s = 70 / z / z
+		
+		circle((C.x * (z + sin(b) * 5 + sin(p) * Q) / z), (C.y + C.x * (cos(q)- cos(b+self.t))/z), s)
+	end
+	
+	setWhite()
 end
 
 function StartScene:clear()
